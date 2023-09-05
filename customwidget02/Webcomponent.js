d11f5948cd66aa245f8e988a290172bc8b5fce74
IYOUSTI <issam.yousti@capgemini.com>
2023-09-05 14:41:29 +0200

    [FIX] added html for main component

--- customwidget02/Webcomponent.js
+++ customwidget02/Webcomponent.js
@@ -1,7 +1,73 @@
 (function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = 
-    `<button type="button" id="myBtn">Helper Button</button>` ;   
+    `
+    <head>
+    <title>Horizontal Bar Chart</title>
+    <style>
+    /* CSS styles here */
+    .horizontal-barchart {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
+    }
+
+    .label {
+      background-color: #5A5A5A;
+      height: 100%;
+      width: 10%;
+      text-align: center;
+      margin: auto;
+      padding: 2px 6px;
+      color: white;
+      border-radius: 4px;
+      height: 30%;
+    }
+
+    .horizontal-barchart .bar {
+      background: #0DA58E;
+      height: 80px;
+      margin: 10px;
+    }
+    </style>
+    </head>
+    <body>
+    <div class="widget-container">
+        <h2>Overzicht datameterketting</h2>
+
+        <br />
+
+        <div class="horizontal-barchart">
+
+        <div class="bar" style="width: 100%">
+            <div class="label">70912</div>
+        </div>
+
+        <div class="bar" style="width: 75%">
+            <div class="label">68652</div>
+        </div>
+
+        <div class="bar" style="width: 71%">
+            <div class="label">68626</div>
+        </div>
+
+        <div class="bar" style="width: 65%">
+            <div class="label">68174</div>
+        </div>
+
+        <div class="bar" style="width: 65%">
+            <div class="label">68174</div>
+        </div>
+
+        <div class="bar" style="width: 59%">
+            <div class="label">62266</div>
+        </div>
+
+        </div>
+    </div>
+    </body>
+    ` ;   
    
     class PerformanceHelp extends HTMLElement {
         constructor() {
